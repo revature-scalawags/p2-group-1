d7324d5966030ef83ad2c2a5114682e94408d332
Chris Johnson <cjohn281@outlook.com>
2021-01-17 12:56:00 -0500

    Began building twitter-batch-get and twitter-batch-analysis programs

---
+++ twitter-batch-analysis/src/main/scala/tweet/TweetFields.scala
@@ -0,0 +1,8 @@
+package tweet
+
+// Cases classes used to convert json to Scala objects
+case class Tweet(data: Array[Data], includes: Includes, meta: Meta)
+case class Data(author_id: String, created_at: String, id: String, text: String)
+case class Includes(users: Array[Users])
+case class Users(description: String, id: String, name: String, username: String)
+case class Meta(newest_id: String, oldest_id: String, result_count: Long)
---
+++ twitter-batch-get/src/main/scala/main/Main.scala
@@ -0,0 +1,11 @@
+package main
+
+/**
+  * twitter-batch-get is the program that is intended to gather histroical data from the Twitter API
+  * and store it on the S3 bucket s3://cjohn281-twit-lake/batch/data.json
+  */
+object Main {
+  def main(args: Array[String]): Unit = {
+    // TODO: Everything
+  }
+}
