d6b2eff7b9863f4e314b3dfe7107e313dd2c3f75
cmorg040 <33102191+cmorg040@users.noreply.github.com>
2021-01-21 17:36:29 -0500

    Initial commit for twee streaming

--- build.sbt
+++ build.sbt
@@ -13,6 +13,11 @@ libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.0.1"
 libraryDependencies += "ch.qos.logback" % "logback-classic" % "1.2.3"
 libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
 
+libraryDependencies += "org.apache.spark" %% "spark-streaming" % "3.0.1"
+libraryDependencies += "org.apache.bahir" %% "spark-streaming-twitter" % "2.4.0"
+libraryDependencies += "org.apache.spark" %% "spark-sql-kafka-0-10" % "3.0.1"
+libraryDependencies += "com.danielasfregola" %% "twitter4s" % "7.0"
+
 assemblyMergeStrategy in assembly := {
  case PathList("META-INF", xs @ _*) => MergeStrategy.discard
  case x => MergeStrategy.first
---
+++ src/main/scala/main/Stream.scala
@@ -0,0 +1,37 @@
+package main
+
+import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.streaming.twitter.TwitterUtils
+import org.apache.spark.streaming.{Duration, StreamingContext}
+
+import com.danielasfregola.twitter4s.TwitterRestClient
+import com.danielasfregola.twitter4s.TwitterStreamingClient
+import com.danielasfregola.twitter4s.entities.Tweet
+import com.danielasfregola.twitter4s.entities.{AccessToken, ConsumerToken}
+
+import com.typesafe.scalalogging.LazyLogging
+
+
+object Stream extends LazyLogging {
+  def main(args: Array[String]): Unit = {
+
+    //TwitterStreamingClient will pull Twitter keys from .env variables.
+    //.env MUST CONTAIN THE FOLLOWING, in this exact format. 
+    /*
+        TWITTER_CONSUMER_TOKEN_KEY=API_key
+        TWITTER_CONSUMER_TOKEN_SECRET=API_secret_key
+        TWITTER_ACCESS_TOKEN_KEY=access_token
+        TWITTER_ACCESS_TOKEN_SECRET=access_token_secret
+    */
+
+    val streamingClient = TwitterStreamingClient()
+
+    val trackedWords = Seq("#fortnite", "#glitch")
+
+    streamingClient.filterStatuses(tracks = trackedWords) { case tweet: Tweet =>
+      println(tweet.text)
+    }
+
+    
+  }
+}
