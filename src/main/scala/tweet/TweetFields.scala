0e60fa1348cecb384fb4ec4d49e36fa5dba71cd2
Cameron <camerondev343@gmail.com>
2021-01-26 11:38:54 -0500

    Updated Documentation

--- src/main/scala/tweet/TweetFields.scala
+++ src/main/scala/tweet/TweetFields.scala
@@ -3,9 +3,12 @@ package tweet
 // Cases classes used to convert json to Scala objects
 case class Tweets(data: Array[Tweet], meta: Meta)
 case class Tweet(id: String, text: String)
-case class Meta(newest_id: String, oldest_id: String, result_count: Long, next_token: String)
-
-
+case class Meta(
+    newest_id: String,
+    oldest_id: String,
+    result_count: Long,
+    next_token: String
+)
 
 /* Used for original temp data, may be useful again at some point
 
@@ -14,4 +17,4 @@ case class Data(author_id: String, created_at: String, id: String, text: String)
 case class Includes(users: Array[Users])
 case class Users(description: String, id: String, name: String, username: String)
 case class Meta(newest_id: String, oldest_id: String, result_count: Long)
-*/
+ */
